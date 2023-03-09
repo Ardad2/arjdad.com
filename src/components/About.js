afa37bca7fdc94a30a90c4a50eceb8b6cde1cdd1
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-03-09 16:49:02 -0700

    fixed coloring

--- src/components/About.js
+++ src/components/About.js
@@ -5,10 +5,10 @@ export default function About() {
     <section id="about">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-black">
           Hey, I'm Arjun Dadhwal!👋🏻
           </h1>
-          <p className="mb-8 leading-relaxed">
+          <p className="mb-8 leading-relaxed text-black">
 
           I'm a junior majoring in Computer Science at Arizona State University. I'm passionate about Software Development and enjoy learning more about my field by developing applications that solve real world problems.
           </p>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ export default function Navbar() {
             Arjun Dadhwal
           </a>
         </a>
-        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-black-700	flex flex-wrap items-center text-base justify-center">
         <a href="#skills" className="mr-5 hover:text-white">
             Skills
           </a>
@@ -21,6 +21,11 @@ export default function Navbar() {
           href="#contact"
           className="mr-5 hover:text-white">
           Contact Me
+        </a>
+        <a
+          href=""
+          className="mr-5 hover:text-white">
+          Blog
         </a>
         </nav>
       </div>
