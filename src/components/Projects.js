c63925911c1167941036ae3f5a834411e659a2de
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-03-12 16:34:53 -0700

    created function for returning projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,37 @@
 import React from "react";
 import { projects } from "../data";
 
+var display = 0;
+
+function displayProjects() {
+  return (<React.Fragment>
+          {projects.map((project) => (
+            <a href={project.link} key={project.image} className="sm:w-1/4 sm:h-1/4 p-2">
+              <div className="flex relative">
+                <img
+                  alt="gallery"
+                  className="absolute inset-0 w-full h-full object-cover object-center"
+                  src={project.image}     
+                />
+                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                  <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
+                    {project.subtitle}
+                  </h2>
+                  <h1 className="title-font text-lg font-medium text-white mb-3">
+                    {project.title}
+                  </h1>
+                  <p className="leading-relaxed">{project.description}</p>
+                </div>
+              </div>
+              <p>{project.title}</p>
+            </a>
+          )
+          
+          )}
+
+  </React.Fragment>)
+}
+
 export default function Projects() {
   return (
     <section id="projects" className="text-gray-900 bg-white-900 body-font">
@@ -41,30 +72,9 @@ export default function Projects() {
 
 
         
-        <div className="flex flex-wrap -m-4">
-          {projects.map((project) => (
-            <a
-              href={project.link}
-              key={project.image}
-              className="sm:w-1/4 sm:h-1/4 p-2">
-              <div className="flex relative">
-                <img
-                  alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
-                  src={project.image}     
-                />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
-                    {project.subtitle}
-                  </h2>
-                  <h1 className="title-font text-lg font-medium text-white mb-3">
-                    {project.title}
-                  </h1>
-                  <p className="leading-relaxed">{project.description}</p>
-                </div>
-              </div>
-            </a>
-          ))}
+        return <div className="flex flex-wrap -m-4">
+        {displayProjects()}
+
         </div>
 
 
