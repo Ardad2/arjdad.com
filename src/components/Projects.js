208fa53bf23125878fa921169eb1377c4adba94a
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-03-12 16:48:04 -0700

    added logic to change between project types

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,46 @@
 import React from "react";
 import { projects } from "../data";
 
-var display = 0;
+var display = "Show All";
 
 function displayProjects(display) {
-  if (display == 0)
+  if (display === "Show All")
+  {
+    return (<React.Fragment>
+      {projects.map((project) => (
+
+
+
+         <a href={project.link} key={project.image} className="sm:w-1/4 sm:h-1/4 p-2">
+          <div className="flex relative">
+            <img
+              alt="gallery"
+              className="absolute inset-0 w-full h-full object-cover object-center"
+              src={project.image}     
+            />
+            <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+              <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
+                {project.subtitle}
+              </h2>
+              <h1 className="title-font text-lg font-medium text-white mb-3">
+                {project.title}
+              </h1>
+              <p className="leading-relaxed">{project.description}</p>
+            </div>
+          </div>
+          <p>{project.title}</p>
+        </a>
+
+      )
+      
+      )}
+
+</React.Fragment>)
+  }
+  else if (display != "Show All")
   {
   return (<React.Fragment>
-          {projects.filter((project) => project.type === "Mobile") .map((project) => (
+          {projects.filter((project) => project.type === display) .map((project) => (
 
 
   
@@ -53,9 +86,12 @@ export default function Projects() {
 
           <div className="flex flex-row w-full">
           <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4	flex flex-wrap items-center  text-gray justify-center">
-        <a href="#skills" className="mr-5 hover:text-white">
+
+        <button onClick={() => {display = "frontEnd"}} className="mr-5 hover:text-white">
         <p>Front End</p>
-          </a>
+          </button>
+
+
           <a href="#projects" className="mr-5 hover:text-white">
           <p>Back End</p>
           </a>
