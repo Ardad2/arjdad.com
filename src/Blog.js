48622b8b58ff1dc5387fc543b5bf9fa214653031
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-03-15 01:52:12 -0700

    starting off blog

---
+++ src/Blog.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+
+
+export default function Blog() {
+
+  useEffect( () => {document.title = 'Arjun Dadhwal - Software Developer'}, []);
+
+  return (
+    <main className="text-gray-400 bg-white-100 body-font " >
+      <Navbar />
+      <About/>
+      <Skills />
+      <Projects/>
+      <Contact />
+    </main>
+  );
+}
