cbe02f45cd6edde5a002cde3bf1cde3705b8f409
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-05-05 11:01:18 -0700

    Updates

---
+++ precache-manifest.80fa651a875b708573608339c7b50348.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a71a1c3f97d691e6a9c7",
+    "url": "/AlternateWebsite.github.io/static/css/main.e6d66d59.chunk.css"
+  },
+  {
+    "revision": "a71a1c3f97d691e6a9c7",
+    "url": "/AlternateWebsite.github.io/static/js/main.b7690879.chunk.js"
+  },
+  {
+    "revision": "2a75727c741d8bffc26f",
+    "url": "/AlternateWebsite.github.io/static/js/runtime~main.0e939c0d.js"
+  },
+  {
+    "revision": "b7db053c211b034bea96",
+    "url": "/AlternateWebsite.github.io/static/js/2.68080ab8.chunk.js"
+  },
+  {
+    "revision": "cf70e40e94e26249cafe4fb668fa350e",
+    "url": "/AlternateWebsite.github.io/index.html"
+  }
+];
