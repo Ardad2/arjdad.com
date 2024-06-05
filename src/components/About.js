8c1310b443bb745dde9009bd99a1b55e1be60b3f
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2024-06-05 15:58:40 +0100

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -17,7 +17,7 @@ export default function About() {
           I'm a 4 + 1 graduate student majoring in Computer Science at Arizona State University. I'm passionate about Software Development and enjoy learning more about my field by developing applications that solve real world problems.
           </p>
           <p className="mb-8 leading-relaxed">
-          Actively seeking Software Engineering internship opportunities for the Summer of 2024.
+          Actively seeking Full Time Software Engineering opportunities for the Summer of 2025.
           </p>
                   <Link to="https://drive.google.com/file/d/1ASG6yt-iyPQWGUIb6FP3H_5rGuz8HQB9/view?usp=sharing" className = "p-2 mr-4 inline-flex px-3 py-2 rounded-lg border-white dark:border-23272f text-gray-700 border rounded-full bg-gray-200 duration-300 dark:text-white transition-colors" >
           Resume
