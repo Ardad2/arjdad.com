2f14b9433286eb5679e2d80756577bfa15c1c0c2
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2024-06-18 09:16:37 +0530

    Add the placeholder component to the blog page.

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -3,6 +3,7 @@ import React, { useEffect } from "react";
 import Navbar from "../components/Navbar";
 import Contact from "../components/Contact";
 //import About from '../components/About';
+import blogPlaceholder from "../components/blogPlaceholder";
 
 export default function Blog() {
 
@@ -11,6 +12,7 @@ export default function Blog() {
   return (
     <main className="text-black dark:text-white bg-white-100 body-font dark:bg-23272f" >
       <Navbar />
+      <blogPlaceholder/>
       <Contact />
     </main>
   );
