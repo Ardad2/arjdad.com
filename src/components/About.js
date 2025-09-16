d4a5aaae840ee4b060d37071234f08ed214247e1
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2025-09-15 23:25:55 -0500

    Fixing typos.

--- src/components/About.js
+++ src/components/About.js
@@ -14,13 +14,13 @@ export default function About() {
           <br/>
           <p className="mb-8 leading-relaxed">
 
-          I'm curently a <b>first-year Computer Science PhD student at Tulane University</b>, working as a <b>Research Asisstant</b> under the supervision of <b>Prof. Brian Summa</b>.
+          I'm currently a <b>first-year Computer Science PhD student at Tulane University</b>, working as a <b>Research Assistant</b> under the supervision of <b>Prof. Brian Summa</b>.
           
           </p>
 
           <p className="mb-8 leading-relaxed">
           
-          I graduated from <b>Arizona State University</b> with an <b>accelerated 4+1 Master’s degree in Computer Science</b> with a specialization in <b>Artificial Intelligence</b>. 
+          I graduated from <b>Arizona State University</b> (ASU) with an <b>accelerated 4+1 Master’s degree in Computer Science</b> with a specialization in <b>Artificial Intelligence</b> in May 2025. 
 
           </p>
                   <Link to="https://drive.google.com/file/d/1ASG6yt-iyPQWGUIb6FP3H_5rGuz8HQB9/view?usp=sharing" className = "p-2 mr-4 inline-flex px-3 py-2 rounded-lg border-white dark:border-23272f text-gray-700 border rounded-full bg-gray-200 duration-300 dark:text-white transition-colors" >
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import Contact from "../components/Contact";
 import Navbar from "../components/Navbar";
 import Projects from "../components/Projects";
 import Skills from "../components/Skills";
+import Education from '../components/Education';
 
 
 export default function Home() {
